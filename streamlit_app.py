2fe15a727f7ed624d980ea03007584c4ab3061a5
RutujaBhuwad <113513843+RutujaBhuwad@users.noreply.github.com>
2023-01-08 15:56:45 +0000

    Update streamlit_app.py
    add new text >>v2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 
 import streamlit
 streamlit.title('my parents  new healthy diner')
+streamlit.head('breakfast menu')
+streamlit.body('item1 x')
+streamlit.body('item1 a')
+streamlit.body('item1 c')
+
